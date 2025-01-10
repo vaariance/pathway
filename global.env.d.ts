7840f49c34cecae4c9471715a802e60a6ad36eb9
Peter Anyaogu <anyaogupeter601@gmail.com>
2025-01-11 00:21:07 +0100

    chore: update globals

--- global.env.d.ts
+++ global.env.d.ts
@@ -3,15 +3,16 @@ declare const process: {
     NODE_ENV: "development" | "production";
     MESSAGE_TABLE: string;
     API_KEY_TABLE: string;
+    EXECUTION_QUEUE_URL: string;
     ATTESTATION_QUEUE_URL: string;
     RELAY_QUEUE_URL: string;
     RETRY_QUEUE_URL: string;
     APITOOLKIT_API_KEY: string;
-    NOBLE_RPC_URL: string;
     DESTINATION_CALLER_API_KEY: string;
     PIMLICO_API_KEY: string;
     NEXT_PUBLIC_PIMLICO_API_KEY: string;
     THIRDWEB_SECRET: string;
+    THIRDWEB_CLIENT_ID: string;
     PATHWAY_API_KEY: string;
     NEXT_PUBLIC_PATHWAY_API_KEY: string;
   };
